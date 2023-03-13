3425575681c0a192dcce13ea6b7c507747c40437
samuelfash7 <edwardsamuel@gmail.com>
2023-03-13 15:54:44 -0700

    task 1

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,16 @@
-#!/bin/bash 
-gcc -Wall -pedantic -Werror -Wextra -c *.c
-ar rc liball.a *.o
+#!/b in/bash
+
+# Collect all the .c files in the current directory
+c_files=$(ls *.c)
+
+# Compile all the .c files into object files
+for file in $c_files
+do
+	gcc -c $file
+done
+
+    # Create a static library from the object files
+    ar rcs liball.a *.o
+
+    # Clean up the object files
+    rm *.o
